d07d1af146152008d55f4a6f43417b1094ce93e1
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 08:30:02 +0100

    modify hello world

---
+++ src/main/java/com/agnes/Main.java
@@ -0,0 +1,9 @@
+package com.agnes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Agnes" );
+        System.out.println("Nazie!");
+        // modified the program to print my name
+    }
+}
