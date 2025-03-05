c9be919f65f57206186cd3e1f440743634dd9cd5
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 10:26:37 +0100

    modify text

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Agnes" );
         System.out.println("Nazie!");
+        System.out.println("54");
         // modified the program to print my name
     }
 }
