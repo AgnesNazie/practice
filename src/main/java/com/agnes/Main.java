d222ccd4906c410d9947920cedef33a597465160
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-10 11:42:04 +0100

    practice important string methods

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -2,7 +2,46 @@ package com.agnes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("sweden");
+
+        // practicing important String methods that are useful for beginners.
+
+        // 1) length of a string
+
+        String name = " Agnes Nazie";
+        int length = name.length();
+
+        // 2) chartAt of a string
+        char letter= name.charAt(6);
+
+       // 3) variable of the index
+        int index = name.indexOf("N");
+
+        // 4) how to use the lastIndex method
+        int lastIndex = name.lastIndexOf("a");
+
+        // 5) how to make strings uppercase.
+        name=name.toUpperCase();
+
+        //6) how to make strings lowercases.
+        name=name.toLowerCase();
+
+        //7) how to use trim method.
+        name=name.trim();
+
+        //8) how to replace character with another.
+        name=name.replace("e", "i");
+
+        //9) how to add IsEmpty method to a string. it is used to return a boolean value. it is very useful with an if statement.
+
+
+        System.out.println(length);
+        System.out.println(letter);
+        System.out.println(index);
+        System.out.println(lastIndex);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+
+
     }
 }
